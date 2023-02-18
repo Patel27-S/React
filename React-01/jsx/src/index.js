da0e93004074ccf8fa6fccc829f2646467be6e14
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2023-02-17 19:13:16 -0500

    My First React App : Hi there!

---
+++ React-01/jsx/src/index.js
@@ -0,0 +1,21 @@
+// This is the first .js file that gets executed in the
+// React App.
+
+// 1) Import React and React DOM Libraries
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// 2) Get a reference to the div with the ID Root+
+const el = document.getElementById('root');
+
+// 3) Tell React to take control of that elememt
+const root = ReactDOM.createRoot(el);
+
+// 4) Create a Component.
+// A component is a function that returns some jsx
+function App(){
+    return <h1>Hi there</h1>;
+}
+
+// 5) Show the component on screen.
+root.render(<App/>);
