92b174eb26bf5d8b6225b8b547dd963ceee5f2c0
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2023-02-17 19:24:03 -0500

    Changed Component Content. Used a JavaScript Variable in the jsx return of a component.

--- React-01/jsx/src/index.js
+++ React-01/jsx/src/index.js
@@ -14,7 +14,13 @@ const root = ReactDOM.createRoot(el);
 // 4) Create a Component.
 // A component is a function that returns some jsx
 function App(){
-    return <h1>Hi there</h1>;
+
+    let message = 'Hey, What is up? This is Smit.'
+
+    if(Math.random > 0.1){
+        message = 'I hope you are doing great!'
+    }
+    return <h1>{message}</h1>;
 }
 
 // 5) Show the component on screen.
