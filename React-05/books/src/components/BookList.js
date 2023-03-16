d2ac434c2d1052b04d364d1d5ceca14714852cfb
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2023-03-15 20:26:50 -0400

    React-6 added

--- React-05/books/src/components/BookList.js
+++ React-05/books/src/components/BookList.js
@@ -1,16 +1,15 @@
-import BookShow from "./BookShow"
+import { useContext } from 'react';
+import BooksContext from '../context/books';
+import BookShow from './BookShow';
 
-function BookList({books, onDelete, onEdit}){
+function BookList() {
+  const { books } = useContext(BooksContext);
 
-    const renderedBooks = books.map((book, index)=>{
-        return <BookShow key = {book.id} book={book} onDelete={onDelete} onEdit={onEdit}/>
-    });
+  const renderedBooks = books.map((book) => {
+    return <BookShow key={book.id} book={book} />;
+  });
 
-    return (
-        <div className="book-list">
-            {renderedBooks}
-        </div>
-    )
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
---
+++ React-06/comps/src/App.js
@@ -0,0 +1,31 @@
+import {GoBell, GoCloudDownload} from 'react-icons/go';
+import Button from "./Button";
+
+function App(){
+
+    const handleClick = () =>{
+        console.log('Please Click This.')
+    }
+    return (<div>
+        <div>
+            <Button success rounded onClick={handleClick}>
+                <GoBell className='mr-1'/>
+                Click me
+            </Button>
+        </div>
+        <div>
+            <Button primary outline>
+                <GoCloudDownload/>
+                Hide Ads
+            </Button>
+        </div>
+        <div>
+            <Button danger>
+                Buy Now
+            </Button>
+        </div>
+    </div>);    
+}
+
+
+export default App;
