37ee8890a461f298d2f912ed67d291a3a193f9e3
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2023-04-17 19:42:11 -0400

    Navigation in a SPA in progress.

---
+++ React-06/comps/src/context/navigation.js
@@ -0,0 +1,40 @@
+import { createContext, useEffect, useState } from "react";
+
+
+const NavigationContext = createContext();
+
+function NavigationProvider({children}) {
+
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(()=>{
+        const handler = () =>{
+
+        };
+        window.addEventListener('popstate', handler);
+
+        return () =>{
+            window.removeEventListener('popstate', handler);
+        };
+    }, []);
+
+
+    const navigate = (to)=>{
+        window.history.pushState({}, '', to);
+        setCurrentPath(to);
+    };
+
+    return (
+    <NavigationContext.Provider value={{ currentPath, navigate}}>
+        <div>
+            <button type="" onClick={() => navigate('/accordion')}>Go to accordion</button>
+            <button type="" onClick={() => navigate('/dropdown')}>Go to dropdown</button>
+        </div>
+        {currentPath}
+        {children}
+    </NavigationContext.Provider>
+    );
+};
+
+export {NavigationProvider};
+export default NavigationContext;
