365d1ef63e16a714c4b5484fb5e66e5c2284399a
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2023-05-03 10:43:34 -0400

    Add Table Component to the Project, Create a CustomHook for the Table Component. Create Counter CustomHook for Learning Purpose.

--- React-06/comps/src/components/Sidebar.js
+++ React-06/comps/src/components/Sidebar.js
@@ -1,35 +1,36 @@
 import Link from "./Link";
 
-function Sidebar(){
+function Sidebar() {
 
     // An array of objects :-
-    const links = [
-        {label: 'Dropdown', path: '/'},
-        {label: 'Accordion', path: '/accordion'},
-        {label: 'Buttons', path: '/buttons'},
-        {label: 'Modal', path: '/modal'},
-    ];
+  const links = [
+    { label: 'Dropdown', path: '/' },
+    { label: 'Accordion', path: '/accordion' },
+    { label: 'Buttons', path: '/buttons' },
+    { label: 'Modal', path: '/modal' },
+    { label: 'Table', path: '/table' },
+    { label: 'Counter', path: '/counter' },
+  ];
 
-    // "map" is a function for arrays. "filter" is also a function on arrays.
-    const renderedLinks = links.map((link)=>{
-        return (
-        <Link 
-            key={link.label} 
-            to={link.path} 
-            children={link.label} 
-            className="mb-3" 
-            activeClassName="font-bold border-l-4 border-blue-500 pl-2"
-        >
-            {link.label}
-        </Link>)
-    })
-
+  // "map" is a function for arrays. "filter" is also a function on arrays.
+  const renderedLinks = links.map((link) => {
     return (
-        <div className="sticky top-0 overflow-y-scroll flex flex-col">
-            {renderedLinks}
-        </div>
+      <Link
+        key={link.label}
+        to={link.path}
+        className="mb-3"
+        activeClassName="font-bold border-l-4 border-blue-500 pl-2"
+      >
+        {link.label}
+      </Link>
     );
-}
+  });
 
+  return (
+    <div className="sticky top-0 overflow-y-scroll flex flex-col items-start">
+      {renderedLinks}
+    </div>
+  );
+}
 
 export default Sidebar;
